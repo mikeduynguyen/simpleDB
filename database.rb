ca64be70f570319172f4db12c3714c9fbdea0b07
Mike Nguyen <mnguyen@interapthq.com>
2016-06-21 15:47:03 -0400

    Finished

--- database.rb
+++ database.rb
@@ -6,24 +6,23 @@ class Database
 	COMMAND_GET = 'GET'
 	COMMAND_UNSET = 'UNSET'
 	COMMAND_NUMEQUALTO = 'NUMEQUALTO'
+	COMMAND_BEGIN = 'BEGIN'
+	COMMAND_ROLLBACK = 'ROLLBACK'
+	COMMAND_COMMIT = 'COMMIT'
 	COMMAND_END = 'END'
+	## No transaction message
+	NO_TRANSACTION = 'NO TRANSACTION'
 	## NULL
 	NULL_VALUE = '> NULL'
 	## Output indicator
 	OUTPUT_INDICATOR = '> '
 	
 	def initialize
-		# Init default transaction for the db, 
-		# which always lives at the bottom of the
-		# stack permanenly.
-		@default_transaction = Transaction.new
+		# Hash to store data
+		@data = {}
 
 		# Transaction stack (LIFO)
-		@transaction_stack = Stack.new
-		@transaction_stack.push(@default_transaction)
-
-		@data = @default_transaction.data
-		@current_transaction = nil
+		@transaction_stack = []
 	end
 
 	# Execute command
@@ -31,14 +30,30 @@ class Database
 	# args[1]: name
 	# args[2]: value
 	def execute(args)
+		# Switch command
 		case args[0]
+
 		when COMMAND_SET
 			# Check if name & value are nil
 			if args.size == 3
+				# Check if in a transaction block
+				if @transaction_stack.size > 0
+					if @data.key?(args[1])
+						# If existing data is being
+						# overwritten save its current state
+						@transaction_stack.last.save_data_state(name, @data[args[1]])
+					else
+						# else keep a record that 
+						# it's a new data entry
+						@transaction_stack.last.insert_new_key(args[1])
+					end		
+				end
+						
 				set(args[1], args[2])
 			else
 				invalid_command
 			end
+
 		when COMMAND_GET
 			# Check if name is nil
 			if args.size == 2
@@ -46,13 +61,24 @@ class Database
 			else
 				invalid_command
 			end
+
 		when COMMAND_UNSET
 			# Check if name is nil
 			if args.size == 2
+				# Check if in a transaction block
+				if @transaction_stack.size > 0
+					if @data.key?(args[1])
+						# If existing data is being
+						# overwritten save its current state
+						save_state(args[1])						
+					end		
+				end	
+
 				unset(args[1])
 			else
 				invalid_command
 			end
+
 		when COMMAND_NUMEQUALTO
 			# Check if name is nil
 			if args.size == 2
@@ -60,8 +86,42 @@ class Database
 			else
 				invalid_command
 			end
+
+		when COMMAND_BEGIN
+			# Create new transaction and
+			# push to stack
+			@transaction_stack.push(Transaction.new)
+
+		when COMMAND_COMMIT
+			if @transaction_stack.size == 0
+				puts OUTPUT_INDICATOR + NO_TRANSACTION
+			else
+				# Commit by clearing the transaction 
+				# stack, doing this will erase all the
+				# saved data. Hence the current data
+				# changes become permanent
+				@transaction_stack.clear
+			end
+
+		when COMMAND_ROLLBACK
+			if @transaction_stack == 0
+				puts OUTPUT_INDICATOR + NO_TRANSACTION
+			else
+				# Rollback changes by writing saved data
+				# from the transaction to the db data
+				@transaction_stack.last.saved_data.each do |name, value|
+					self.set(name, value)
+				end
+				# and delete newly created data
+				@transaction_stack.last.new_data.each do |name|
+					self.unset(name)
+				end
+				@transaction_stack.pop
+			end
+
 		when COMMAND_END
 			exit
+
 		else
 			invalid_command
 		end
@@ -74,56 +134,56 @@ class Database
 	end
 
 	private
-	def set(key, value)
-		@data[key] = value
+	def set(name, value)
+		@data[name] = value
 	end
 
-	def get(key)
-		unless @data[key].nil?
-			puts OUTPUT_INDICATOR + @data[key]
+	def get(name)
+		unless @data[name].nil?
+			puts OUTPUT_INDICATOR + @data[name]
 		else
 			puts NULL_VALUE
 		end		
 	end
 
-	def unset(key)
-		@data[key] = nil
+	def unset(name)
+		@data[name] = nil
 	end
 
 	# Count number of occurences for a value
 	def num_equal_to(value)
 		puts OUTPUT_INDICATOR + @data.values.count(value).to_s
 	end
+
 end
 
 #-------- Database transaction class --------#
 class Transaction	
+	attr_reader :saved_data
+	attr_reader :new_data 
+
 	def initialize
-		# @data: hash for storing data
-		@data = {}
-	end
-end
+		# @saved_data: hash for storing current 
+		# version of data for each write 
+		# command
+		@saved_data = {}
 
-#-------- Simple stack implementation --------#
-def Stack
-	def initialize
-		@stack = []
-	end
-
-	def push(item)
-		@stack.push(item)
+		# @new_data: hash for storing new data
+		# keys created in the transaction
+		@new_data = []
 	end
 
-	def pop
-		# Prevent the default transaction
-		# to be popped
-		unless @stack.size == 1
-			@stack.pop
+	def save_data_state(name, value)
+		# Only save current state of
+		# data point, if data is already
+		# saved, do nothing.
+		if @data[name].nil?
+			@data[name] = value
 		end
 	end
 
-	def get_last_item
-		@stack.last 
+	def insert_new_key(name)
+		@new_data << name
 	end
 end
 
